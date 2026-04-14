9566e6c2df85faf945823730299b0051d1507870
Vishal Agrahari <agraharivishal233@gmail.com>
2026-04-14 07:09:59 +0000

    investigation

---
+++ 01_basic/01_conversion.js
@@ -0,0 +1,29 @@
+console.log("Wellcome to this place")
+
+let num = 45
+console.log( typeof num)
+
+// convert into String->
+
+let string = String(num)
+console.log(typeof string)
+console.log(string)
+
+// convert into number-->
+
+let name = "Vishal"
+console.log(typeof name)
+let numname = Number(name)
+console.log(numname)
+console.log(numname)
+
+// Table form
+
+let Name ="Vishal Agrahari"
+let Village = "Turkauliya Tiwari"
+let Course = "B-tech"
+let email_id = "vishal@gmail.com"
+let branch = "Information technology"
+
+console.table([Name,Village,Course,email_id,branch])
+
