6d861e5bb173684e7f95fb347101f8764dfc69c0
Vishal Agrahari <agraharivishal233@gmail.com>
2026-04-16 17:40:02 +0000

    Date update

---
+++ 01_basic/Date.js
@@ -0,0 +1,8 @@
+//Date
+// const myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocalleString());
+
+let myBirthday = new Date(2005,1,16)
+console.log(myBirthday.toDateString());
